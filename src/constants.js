2beb2bc210abe31fd324d35b6bb7764b41c66a02
Paul Petrov <paul@bargamut.ru>
2020-04-22 22:04:44 +0300

    Add constants to src/constants.js

--- src/constants.js
+++ src/constants.js
@@ -2,8 +2,17 @@
 
 const DEFAULT_COUNT = 1;
 const MAX_COUNTS = 1000;
+const DEFAULT_COMMAND = `--help`;
+const USER_ARGV_INDEX = 2;
+const ExitCode = {
+  success: 0,
+  error: 1
+};
 
 module.exports = {
   DEFAULT_COUNT,
   MAX_COUNTS,
+  DEFAULT_COMMAND,
+  USER_ARGV_INDEX,
+  ExitCode,
 };
