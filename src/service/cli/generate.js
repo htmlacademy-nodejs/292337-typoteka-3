bce5d2cd1658a1791806f3869dd17406ddbe11a0
Paul Petrov <paul@bargamut.ru>
2020-04-29 14:55:19 +0300

    Create readContent from ./src/data/ files

--- src/service/cli/generate.js
+++ src/service/cli/generate.js
@@ -14,56 +14,13 @@ const {
   shuffle,
 } = require(`../../utils`);
 
-const TITLES = [
-  `Ёлки. История деревьев`,
-  `Как перестать беспокоиться и начать жить`,
-  `Как достигнуть успеха не вставая с кресла`,
-  `Обзор новейшего смартфона`,
-  `Лучше рок-музыканты 20-века`,
-  `Как начать программировать`,
-  `Учим HTML и CSS`,
-  `Что такое золотое сечение`,
-  `Как собрать камни бесконечности`,
-  `Борьба с прокрастинацией`,
-  `Рок — это протест`,
-  `Самый лучший музыкальный альбом этого года`,
-];
-const PHRASES = [
-  `Ёлки — это не просто красивое дерево. Это прочная древесина.`,
-  `Первая большая ёлка была установлена только в 1938 году.`,
-  `Вы можете достичь всего. Стоит только немного постараться и запастись книгами.`,
-  `Этот смартфон — настоящая находка. Большой и яркий экран, мощнейший процессор — всё это в небольшом гаджете.`,
-  `Золотое сечение — соотношение двух величин, гармоническая пропорция.`,
-  `Собрать камни бесконечности легко, если вы прирожденный герой.`,
-  `Освоить вёрстку несложно. Возьмите книгу новую книгу и закрепите все упражнения на практике.`,
-  `Бороться с прокрастинацией несложно. Просто действуйте. Маленькими шагами.`,
-  `Программировать не настолько сложно, как об этом говорят.`,
-  `Простые ежедневные упражнения помогут достичь успеха.`,
-  `Это один из лучших рок-музыкантов.`,
-  `Он написал больше 30 хитов.`,
-  `Из под его пера вышло 8 платиновых альбомов.`,
-  `Процессор заслуживает особого внимания. Он обязательно понравится геймерам со стажем.`,
-  `Рок-музыка всегда ассоциировалась с протестами. Так ли это на самом деле?`,
-  `Достичь успеха помогут ежедневные повторения.`,
-  `Помните, небольшое количество ежедневных упражнений лучше, чем один раз, но много.`,
-  `Как начать действовать? Для начала просто соберитесь.`,
-  `Игры и программирование разные вещи. Не стоит идти в программисты, если вам нравится только игры.`,
-  `Альбом стал настоящим открытием года. Мощные гитарные рифы и скоростные соло-партии не дадут заскучать.`,
-];
-const CATEGORIES = [
-  `Деревья`,
-  `За жизнь`,
-  `Без рамки`,
-  `Разное`,
-  `IT`,
-  `Музыка`,
-  `Кино`,
-  `Программирование`,
-  `Железо`,
-];
+const FILE_MOCK_PATH = `./mock.json`;
+const FILE_TITLES_PATH = `./src/data/titles.txt`;
+const FILE_SENTENCES_PATH = `./src/data/sentences.txt`;
+const FILE_CATEGORIES_PATH = `./src/data/categories.txt`;
 
-const getItemTitle = () => {
-  return TITLES[getRandomInt(0, TITLES.length - 1)];
+const getItemTitle = (titles) => {
+  return titles[getRandomInt(0, titles.length - 1)];
 };
 
 const getItemCreatedDate = () => {
@@ -75,25 +32,39 @@ const getItemCreatedDate = () => {
     .toFormat(`yyyy-MM-dd HH:mm:ss`);
 };
 
-const getItemAnnounce = () => shuffle(PHRASES).slice(1, getRandomInt(2, 5)).join(` `);
+const getItemAnnounce = (sentences) => shuffle(sentences).slice(1, getRandomInt(2, 5)).join(` `);
 
-const getItemFullText = () => shuffle(PHRASES).slice(1, getRandomInt(2, PHRASES.length - 1)).join(` `);
+const getItemFullText = (sentences) => shuffle(sentences).slice(1, getRandomInt(2, sentences.length - 1)).join(` `);
 
-const getItemCategories = () => shuffle(CATEGORIES).slice(1, getRandomInt(2, CATEGORIES.length - 1));
+const getItemCategories = (categories) => shuffle(categories).slice(1, getRandomInt(2, categories.length - 1));
 
-const generateItems = (count) => {
+const generateItems = (count, titles, categories, sentences) => {
   return Array(count).fill().map(() => ({
-    title: getItemTitle(),
+    title: getItemTitle(titles),
     createdDate: getItemCreatedDate(),
-    announce: getItemAnnounce(),
-    fullText: getItemFullText(),
-    category: getItemCategories(),
+    announce: getItemAnnounce(sentences),
+    fullText: getItemFullText(sentences),
+    category: getItemCategories(categories),
   }));
+};
+
+const readContent = async (filePath) => {
+  try {
+    const content = await fs.readFile(filePath, `utf8`);
+
+    return content.split(`\n`);
+  } catch (error) {
+    console.error(
+        chalk.red(error)
+    );
+
+    return [];
+  }
 };
 
 const saveToMock = async (content) => {
   try {
-    await fs.writeFile(`mock.json`, content);
+    await fs.writeFile(FILE_MOCK_PATH, content);
 
     console.info(
         chalk.green(`Operation success. File created.`)
@@ -107,7 +78,7 @@ const saveToMock = async (content) => {
 
 module.exports = {
   name: `--generate`,
-  run(args) {
+  async run(args) {
     const [count] = args;
     const countItem = Number.parseInt(count, 10) || DEFAULT_COUNT;
 
@@ -119,7 +90,18 @@ module.exports = {
       return ExitCode.error;
     }
 
-    const content = JSON.stringify(generateItems(countItem));
+    const titles = await readContent(FILE_TITLES_PATH);
+    const categories = await readContent(FILE_CATEGORIES_PATH);
+    const sentences = await readContent(FILE_SENTENCES_PATH);
+
+    const content = JSON.stringify(
+        generateItems(
+            countItem,
+            titles,
+            categories,
+            sentences
+        )
+    );
 
     saveToMock(content);
 
