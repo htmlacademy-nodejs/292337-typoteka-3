2675bf8010eb651869b2afa72de2fafc4c877959
Paul Petrov <paul@bargamut.ru>
2020-05-07 18:37:12 +0300

    Create express/index.js HTTP server

---
+++ src/express/index.js
@@ -0,0 +1,19 @@
+'use strict';
+
+const express = require(`express`);
+
+const mainRouter = require(`./routes/main-routes`);
+const myRouter = require(`./routes/my-routes`);
+const articlesRouter = require(`./routes/articles-routes`);
+
+const DEFAULT_PORT = 8080;
+
+const app = express();
+
+app.use(`/`, mainRouter);
+app.use(`/my`, myRouter);
+app.use(`/articles`, articlesRouter);
+
+app.listen(DEFAULT_PORT, ()=> {
+  console.log(`Сервер стартовал на http://localhost:${DEFAULT_PORT}`);
+});
