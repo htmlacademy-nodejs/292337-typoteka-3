b695c2c374754add8d955cde714469d96e14bbb9
Paul Petrov <paul@bargamut.ru>
2020-05-10 17:58:50 +0300

    Create template & route for main route

--- src/express/routes/main-routes.js
+++ src/express/routes/main-routes.js
@@ -4,7 +4,7 @@ const {Router} = require(`express`);
 
 const mainRouter = new Router();
 
-mainRouter.get(`/`, (req, res) => res.send(`/`));
+mainRouter.get(`/`, (req, res) => res.render(`main`));
 mainRouter.get(`/register`, (req, res) => res.send(`/register`));
 mainRouter.get(`/login`, (req, res) => res.send(`/login`));
 mainRouter.get(`/search`, (req, res) => res.send(`/search`));
