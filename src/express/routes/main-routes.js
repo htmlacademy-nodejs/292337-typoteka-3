1a8f6a40cdb08e306739a919a7eac269aa04aa87
Paul Petrov <paul@bargamut.ru>
2020-05-10 18:22:10 +0300

    Create template & route for /search

--- src/express/routes/main-routes.js
+++ src/express/routes/main-routes.js
@@ -7,7 +7,7 @@ const mainRouter = new Router();
 mainRouter.get(`/`, (req, res) => res.render(`main`));
 mainRouter.get(`/register`, (req, res) => res.render(`sign-up`));
 mainRouter.get(`/login`, (req, res) => res.render(`login`));
-mainRouter.get(`/search`, (req, res) => res.send(`/search`));
+mainRouter.get(`/search`, (req, res) => res.render(`search`));
 mainRouter.get(`/categories`, (req, res) => res.send(`/categories`));
 
 module.exports = mainRouter;
