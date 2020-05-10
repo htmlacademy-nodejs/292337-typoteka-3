b58901045566f9978a52c08f656f737b474568dc
Paul Petrov <paul@bargamut.ru>
2020-05-10 18:11:36 +0300

    Create template & route for /register

--- src/express/routes/main-routes.js
+++ src/express/routes/main-routes.js
@@ -5,7 +5,7 @@ const {Router} = require(`express`);
 const mainRouter = new Router();
 
 mainRouter.get(`/`, (req, res) => res.render(`main`));
-mainRouter.get(`/register`, (req, res) => res.send(`/register`));
+mainRouter.get(`/register`, (req, res) => res.render(`sign-up`));
 mainRouter.get(`/login`, (req, res) => res.render(`login`));
 mainRouter.get(`/search`, (req, res) => res.send(`/search`));
 mainRouter.get(`/categories`, (req, res) => res.send(`/categories`));
