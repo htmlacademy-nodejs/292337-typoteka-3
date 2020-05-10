26d7b88762d6366bdf9d13e063fbfa692ab26d45
Paul Petrov <paul@bargamut.ru>
2020-05-10 18:41:05 +0300

    Create template & route for /articles/:id

--- src/express/routes/articles-routes.js
+++ src/express/routes/articles-routes.js
@@ -5,7 +5,7 @@ const {Router} = require(`express`);
 const articlesRouter = new Router();
 
 articlesRouter.get(`/add`, (req, res) => res.send(`/articles/add`));
-articlesRouter.get(`/:id`, (req, res) => res.send(`/articles/:id`));
+articlesRouter.get(`/:id`, (req, res) => res.render(`post`));
 articlesRouter.get(`/edit/:id`, (req, res) => res.send(`/articles/edit/:id`));
 articlesRouter.get(`/category/:id`, (req, res) => res.send(`/articles/category/:id`));
 
