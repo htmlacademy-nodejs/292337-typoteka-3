581e29793409683177f94a6c4e0059225bcfd7f1
Paul Petrov <paul@bargamut.ru>
2020-05-10 19:39:44 +0300

    Create template & route for /my

--- src/express/routes/my-routes.js
+++ src/express/routes/my-routes.js
@@ -4,7 +4,7 @@ const {Router} = require(`express`);
 
 const myRouter = new Router();
 
-myRouter.get(`/`, (req, res) => res.send(`/my/`));
+myRouter.get(`/`, (req, res) => res.render(`admin/my`));
 myRouter.get(`/comments`, (req, res) => res.render(`admin/comments`));
 
 module.exports = myRouter;
