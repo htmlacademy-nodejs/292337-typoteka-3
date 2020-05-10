30c5514b6ee6b42da1a713be77e6aeffe3f64c0f
Paul Petrov <paul@bargamut.ru>
2020-05-10 20:01:25 +0300

    Create template & route for /categories

--- src/express/routes/main-routes.js
+++ src/express/routes/main-routes.js
@@ -8,6 +8,6 @@ mainRouter.get(`/`, (req, res) => res.render(`main`));
 mainRouter.get(`/register`, (req, res) => res.render(`sign-up`));
 mainRouter.get(`/login`, (req, res) => res.render(`login`));
 mainRouter.get(`/search`, (req, res) => res.render(`search`));
-mainRouter.get(`/categories`, (req, res) => res.send(`/categories`));
+mainRouter.get(`/categories`, (req, res) => res.render(`admin/all-categories`));
 
 module.exports = mainRouter;
