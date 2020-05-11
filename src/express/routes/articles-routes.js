a0b6789e0d6126628b1e9d8da8005fb8176be5f6
Paul Petrov <paul@bargamut.ru>
2020-05-11 19:58:51 +0300

    Add mock userRole option for some routes

--- src/express/routes/articles-routes.js
+++ src/express/routes/articles-routes.js
@@ -4,9 +4,9 @@ const {Router} = require(`express`);
 
 const articlesRouter = new Router();
 
-articlesRouter.get(`/add`, (req, res) => res.render(`admin/new-post`));
+articlesRouter.get(`/add`, (req, res) => res.render(`admin/new-post`, {userRole: `admin`}));
 articlesRouter.get(`/:id`, (req, res) => res.render(`post`));
-articlesRouter.get(`/edit/:id`, (req, res) => res.render(`admin/edit-post`));
+articlesRouter.get(`/edit/:id`, (req, res) => res.render(`admin/edit-post`, {userRole: `admin`}));
 articlesRouter.get(`/category/:id`, (req, res) => res.render(`articles-by-category`));
 
 module.exports = articlesRouter;
--- src/express/routes/my-routes.js
+++ src/express/routes/my-routes.js
@@ -4,7 +4,7 @@ const {Router} = require(`express`);
 
 const myRouter = new Router();
 
-myRouter.get(`/`, (req, res) => res.render(`admin/my`));
-myRouter.get(`/comments`, (req, res) => res.render(`admin/comments`));
+myRouter.get(`/`, (req, res) => res.render(`admin/my`, {userRole: `admin`}));
+myRouter.get(`/comments`, (req, res) => res.render(`admin/comments`, {userRole: `admin`}));
 
 module.exports = myRouter;
