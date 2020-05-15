f1c3b4ad2ae13299607d09c2f0a227535af6a24a
Paul Petrov <paul@bargamut.ru>
2020-05-15 17:42:00 +0300

    Connect getMockData() to api/index.js

--- src/service/api/index.js
+++ src/service/api/index.js
@@ -2,6 +2,14 @@
 
 const {Router} = require(`express`);
 
+const {getMockData} = require(`../lib/get-mock-data`);
+
 const router = new Router();
+
+(async () => {
+  const mockData = await getMockData();
+
+  // TODO: add api routers
+})();
 
 module.exports = router;
