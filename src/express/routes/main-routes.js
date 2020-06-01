c1bb8777ef5041580dfd0609a3279faf9c1cd4ea
Paul Petrov <paul@bargamut.ru>
2020-06-01 15:45:57 +0300

    Refactor main route / of front server

--- src/express/routes/main-routes.js
+++ src/express/routes/main-routes.js
@@ -4,10 +4,16 @@ const {Router} = require(`express`);
 
 const mainRouter = new Router();
 
-mainRouter.get(`/`, (req, res) => res.render(`main`));
-mainRouter.get(`/register`, (req, res) => res.render(`sign-up`));
-mainRouter.get(`/login`, (req, res) => res.render(`login`));
-mainRouter.get(`/search`, (req, res) => res.render(`search`));
-mainRouter.get(`/categories`, (req, res) => res.render(`admin/all-categories`, {userRole: `admin`}));
+module.exports = (parentRouter, articlesDataService) => {
+  parentRouter.use(`/`, mainRouter);
 
-module.exports = mainRouter;
+  mainRouter.get(`/`, async (req, res) => {
+    const articles = await articlesDataService.findAll();
+
+    res.render(`main`, {articles});
+  });
+  mainRouter.get(`/register`, (req, res) => res.render(`sign-up`));
+  mainRouter.get(`/login`, (req, res) => res.render(`login`));
+  mainRouter.get(`/search`, (req, res) => res.render(`search`));
+  mainRouter.get(`/categories`, (req, res) => res.render(`admin/all-categories`, {userRole: `admin`}));
+};
