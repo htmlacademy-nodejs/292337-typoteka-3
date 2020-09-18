09cb42a6657642fa201fd005ac9689010e59a4bf
Paul Petrov <paul@bargamut.ru>
2020-09-18 17:54:32 +0300

    Update process data middleware

--- src/express/middlewares/process-data-new-article.js
+++ src/express/middlewares/process-data-new-article.js
@@ -1,16 +1,28 @@
 'use strict';
 
-module.exports = (req, res, next) => {
+const adapter = require(`../lib/data-adapter`);
+const {saveFile} = require(`../lib/file-manager`);
+
+const {ALLOWED_FILETYPES} = require(`../../constants`);
+
+module.exports = async (req, res, next) => {
   try {
-    console.log(req.files.titleImage);
+    req.files.titleImage = await saveFile({...req.files.titleImage});
   } catch (error) {
-    return false;
+    return res.render(`admin/new-post`, {
+      allowedFileTypes: ALLOWED_FILETYPES,
+      article: adapter({
+        ...res.fields,
+        ...res.files,
+      }),
+      error: error.message,
+    });
   }
 
-  res.locals.data = {
+  res.locals.data = adapter({
     ...req.fields,
     ...req.files,
-  };
+  });
 
   return next();
 };
